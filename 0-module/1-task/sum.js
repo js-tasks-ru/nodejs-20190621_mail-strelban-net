16506cbe0f829e0ad7726804650bea7078a53ebf
Andrey <strelban@gmail.com>
2019-06-24 21:15:08 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-  /* ваш код */
+  if (isNaN(parseFloat(a)) || isNaN(parseFloat(b)) ) {
+    throw new TypeError('Arguments must be a numbers');
+  }
+  return a + b;
 }
 
 module.exports = sum;
